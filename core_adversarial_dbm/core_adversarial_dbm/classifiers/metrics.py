6a3aba39b24774666116eb9bccfd55d275d1ab42
Alister Machado dos Reis <a.machadodosreis@uu.nl>
2023-07-05 10:50:08 +0200

    Add per-class accuracy metric

--- core_adversarial_dbm/core_adversarial_dbm/classifiers/metrics.py
+++ core_adversarial_dbm/core_adversarial_dbm/classifiers/metrics.py
@@ -1,9 +1,23 @@
 import torch as T
 import torch.nn as nn
+import numpy as np
+
+from core_adversarial_dbm.defs import DEVICE
+
 
 @T.no_grad()
-def accuracy(classifier: nn.Module, X, y, device):
+def accuracy(classifier: nn.Module, X, y, device=DEVICE):
     outputs = classifier.classify(T.tensor(X, device=device))
 
     return (outputs.cpu().numpy() == y).mean()
 
+
+@T.no_grad()
+def accuracy_per_class(classifier: nn.Module, X, y, device=DEVICE):
+    outputs = classifier.classify(T.tensor(X, device=device)).cpu().numpy()
+
+    classes = np.unique(y)
+    accs = np.zeros(len(classes), dtype=np.float32)
+    for cl in classes:
+        accs[cl] = (outputs[y == cl] == cl).mean()
+    return accs
